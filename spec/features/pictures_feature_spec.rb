00187803bfe30d35d5ec0ca1fde4d3f1e4cd6b6d
kpuwal <kpuwal@gmail.com>
2016-07-18 01:45:56 +0100

    adds all CRUD features

---
+++ spec/features/pictures_feature_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+feature 'pictures' do
+  context 'no pictures have been added' do
+    scenario 'should display a prompt to add a picture' do
+      visit '/pictures'
+      expect(page).to have_content 'No pictures yet'
+      expect(page).to have_link('Add Picture')
+    end
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Picture.create(title: 'sky')
+    end
+
+    scenario 'display pictures' do
+      visit '/pictures'
+      expect(page).to have_content('sky')
+      expect(page).not_to have_content('No pictures yet')
+    end
+  end
+
+  context 'creating picture' do
+    scenario 'prompts user to fill out a form, then displays the new picture' do
+      visit '/pictures'
+      click_link 'Add Picture'
+      fill_in 'Title', with: 'sky'
+      fill_in 'Description', with: 'Deep blue sky'
+      click_button 'Create Picture'
+      expect(page).to have_content 'sky'
+      expect(page).to have_content 'Deep blue sky'
+      expect(current_path).to eq '/pictures'
+    end
+  end
+
+  context 'viewing pictures' do
+
+    let!(:sky){ Picture.create(title:'sky', description: 'Deep blue sky') }
+
+    scenario 'lets a user view pictures' do
+     visit '/pictures'
+     click_link 'sky'
+     expect(page).to have_content 'sky'
+     expect(page).to have_content 'Deep blue sky'
+     expect(current_path).to eq "/pictures/#{sky.id}"
+    end
+  end
+
+  context 'editing pictures' do
+
+    before { Picture.create title: 'sky' }
+
+    scenario 'let a user edit a picture' do
+     visit '/pictures'
+     click_link 'Edit sky'
+     fill_in 'Title', with: 'sky'
+     fill_in 'Description', with: 'Deep blue sky'
+     click_button 'Update Picture'
+     expect(page).to have_content 'sky'
+     expect(page).to have_content 'Deep blue sky'
+     expect(current_path).to eq '/pictures'
+    end
+  end
+
+  context 'deleting pictures' do
+
+    before { Picture.create title: 'sky' }
+
+    scenario 'removes a picture when a user clicks a delete link' do
+      visit '/pictures'
+      click_link 'Delete sky'
+      expect(page).not_to have_content 'sky'
+      expect(page).to have_content 'Picture deleted successfully'
+    end
+
+  end
+end
